27ce24dd04b4f8af713ebca506b4a6cea462ea8c
FeNjK <bodhisatva_xp@mail.ru>
2022-09-12 03:27:37 +0300

    fix: add celebrate-validation.

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post(
       name: Joi.string().required().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().regex(
-        /https?:\/\/(www)?[-a-zA-Z0-9@:%_+.~#?&=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&=]*)?/gi,
+        /https?:\/\/(www)?[-a-zA-Z0-9@:%_+.~#?&=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&=]*)?/i,
       ),
     }),
   }),
